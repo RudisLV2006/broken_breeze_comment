6346e1012242ccea34b1b328910412ff3f3f8f91
Vigo28 <gamingvigo06@gmail.com>
2025-03-24 16:26:05 +0200

    Show all comments

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,13 +9,13 @@ use Illuminate\Support\Facades\Storage;
 class PostController extends Controller
 {
 
-    
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::with('comments')->get();
         return view('post.index', ['posts' => $posts]);
     }
 
